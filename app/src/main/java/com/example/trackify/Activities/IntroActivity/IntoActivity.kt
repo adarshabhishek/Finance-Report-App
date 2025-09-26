ae15b8d3aba1b16eaa25bd6d73889118c9396451
Adarsh Abhishek <adarshabhishek1203@gmail.com>
2025-09-27 00:01:39 +0530

    Into Screen

---
+++ app/src/main/java/com/example/trackify/Activities/IntroActivity/IntoActivity.kt
@@ -0,0 +1,22 @@
+package com.example.trackify.Activities.IntroActivity
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import com.example.trackify.Activities.IntroActivity.screens.IntroScreen
+import com.example.trackify.MainActivity
+
+class IntoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            IntroScreen(onStartClick = {
+                startActivity(Intent(this, MainActivity::class.java))
+            })
+        }
+
+    }
+}
