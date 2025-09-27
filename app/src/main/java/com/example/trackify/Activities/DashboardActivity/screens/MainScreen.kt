80eb1968c4a979bc7353a1d50d22886c37edf16a
Adarsh Abhishek <adarshabhishek1203@gmail.com>
2025-09-27 19:53:44 +0530

    ExpenseItem List

--- app/src/main/java/com/example/trackify/Activities/DashboardActivity/screens/MainScreen.kt
+++ app/src/main/java/com/example/trackify/Activities/DashboardActivity/screens/MainScreen.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -13,6 +14,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.trackify.Activities.DashboardActivity.components.ActionButtonRow
 import com.example.trackify.Activities.DashboardActivity.components.CardSection
+import com.example.trackify.Activities.DashboardActivity.components.ExpenseItem
 import com.example.trackify.Activities.DashboardActivity.components.HeaderSection
 import com.example.trackify.Domain.ExpenseDomain
 
@@ -20,10 +22,10 @@ import com.example.trackify.Domain.ExpenseDomain
 @Preview(showBackground = true)
 fun MainScreenPreview() {
     val expenses = listOf(
-        ExpenseDomain("Restaurant", 573.12, "img1", "27 Sep 2025 22:15"),
-        ExpenseDomain("McDonalds", 180.12, "img2", "26 Sep 2025 13:10"),
-        ExpenseDomain("Movie", 238.00,"img3", "26 Sep 2025 11:25"),
-        ExpenseDomain("Groceries", 456.18, "img4", "25 Sep 2025 12:10")
+        ExpenseDomain("Restaurant", 573.12, "restaurant", "27 Sep 2025 22:15"),
+        ExpenseDomain("McDonalds", 180.12, "mcdonald", "26 Sep 2025 13:10"),
+        ExpenseDomain("Movie", 238.00,"cinema", "26 Sep 2025 11:25"),
+        ExpenseDomain("Restaurant", 456.18, "restaurant", "25 Sep 2025 12:10")
     )
 
     MainScreen(expenses=expenses)
@@ -45,6 +47,9 @@ fun MainScreen(
             item{ HeaderSection() }
             item { CardSection (onCardClick) }
             item { ActionButtonRow() }
+            items(expenses){
+                item-> ExpenseItem(item)
+            }
         }
     }
 }
--- app/src/main/java/com/example/trackify/Repository/MainRepository.kt
+++ app/src/main/java/com/example/trackify/Repository/MainRepository.kt
@@ -4,9 +4,9 @@ import com.example.trackify.Domain.ExpenseDomain
 
 class MainRepository {
     val items = mutableListOf(
-        ExpenseDomain("Restaurant", 573.12, "img1", "27 Sep 2025 22:15"),
-        ExpenseDomain("McDonalds", 180.12, "img2", "26 Sep 2025 13:10"),
-        ExpenseDomain("Movie", 238.00,"img3", "26 Sep 2025 11:25"),
-        ExpenseDomain("Groceries", 456.18, "img4", "25 Sep 2025 12:10")
+        ExpenseDomain("Restaurant", 573.12, "restaurant", "27 Sep 2025 22:15"),
+        ExpenseDomain("McDonalds", 180.12, "mcdonald", "26 Sep 2025 13:10"),
+        ExpenseDomain("Movie", 238.00,"cinema", "26 Sep 2025 11:25"),
+        ExpenseDomain("Restaurant", 456.18, "restaurant", "25 Sep 2025 12:10")
     )
 }
