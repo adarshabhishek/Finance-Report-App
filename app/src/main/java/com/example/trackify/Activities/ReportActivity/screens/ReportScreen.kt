1ff50221101d8a5ac6f3f20336b1faa461cfa06b
Adarsh Abhishek <adarshabhishek1203@gmail.com>
2025-10-02 11:26:07 +0530

    StatsCard & ReportScreen

--- app/src/main/java/com/example/trackify/Activities/ReportActivity/screens/ReportScreen.kt
+++ app/src/main/java/com/example/trackify/Activities/ReportActivity/screens/ReportScreen.kt
@@ -4,6 +4,9 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -12,6 +15,7 @@ import androidx.compose.ui.layout.ModifierLocalBeyondBoundsLayout
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.constraintlayout.compose.ConstraintLayout
+import com.example.trackify.Activities.ReportActivity.components.CenterStatsCard
 import com.example.trackify.Activities.ReportActivity.components.GradientHeader
 import com.example.trackify.Domain.BudgetDomain
 
@@ -29,7 +33,6 @@ fun ReportScreen(
                 top.linkTo(parent.top)
                 start.linkTo(parent.start)
                 end.linkTo(parent.end)
-                bottom.linkTo(bottomBarRef.top)
             },
             onBack = onBack
         )
@@ -50,10 +53,10 @@ fun ReportContent(
             ConstraintLayout(
                 modifier = Modifier
                     .fillMaxWidth()
-                    .height(250.dp)
+                    .height(420.dp)
             )
             {
-                val (header, title) = createRefs()
+                val (header, card) = createRefs()
                 GradientHeader(
                     modifier= Modifier
                         .fillMaxWidth()
@@ -61,9 +64,21 @@ fun ReportContent(
                         .constrainAs(header){
                             top.linkTo(parent.top)
                             start.linkTo(parent.start)
-                            end.linkTo(parent.end)
+                            end.linkTo( parent.end)
                         },
                     onBack = onBack
+                )
+                CenterStatsCard(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .wrapContentHeight()
+                        .padding(horizontal = 24.dp)
+                        .constrainAs(card){
+                            top.linkTo(header.bottom)
+                            bottom.linkTo(header.bottom)
+                            start.linkTo(parent.start)
+                            end.linkTo(parent.end)
+                        }
                 )
             }
         }
